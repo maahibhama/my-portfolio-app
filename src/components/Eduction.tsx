bbdac24c2f535763ee9dd4773e0c44391f54112c
Mahendra Bhama <maahibhama@gmail.com>
2023-06-19 20:42:31 +0530

    added education and experience

--- src/components/Eduction.tsx
+++ src/components/Eduction.tsx
@@ -1,35 +1,27 @@
 import Title from "./Title";
-import { MdWork } from "react-icons/md";
+import { MdCastForEducation, MdWork } from "react-icons/md";
 import { SiReactivex, SiToptal, SiFreelancer, SiFiverr } from "react-icons/si";
 import ExperienceCard from "./ExperienceCard";
+import { EducationData } from "@/constants/utils";
+import Image from "next/image";
 
 const Eduction = () => {
   return (
     <div className="wrapper">
-      <Title text="Experiences" icon={<MdWork />} />
+      <Title text="Education" icon={<MdCastForEducation />} />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-        <ExperienceCard
-          title="Bright"
-          subTitle="Lead Engineer 2022 - Present"
-          icon={<SiReactivex />}
-          description={'adad'}
-        />
-        <ExperienceCard
-          title="Toptal"
-          subTitle="MERN Stack Developer 2021 - present"
-          icon={<SiToptal />}
-        />
-        <ExperienceCard
-          title="Freelancer"
-          subTitle="MERN Stack Developer 2020 - 2021"
-          icon={<SiFreelancer />}
-        />
-
-        <ExperienceCard
-          title="Fiverr"
-          subTitle="MERN Stack Developer 2019 - 2020"
-          icon={<SiFiverr />}
-        />
+        {EducationData.map((item, index) => {
+          return (
+            <ExperienceCard
+              key={index}
+              title={item.title}
+              url={item.url}
+              subTitle={item.position}
+              icon={<Image src={item.imageUrl} />}
+              description={item.description}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,20 +1,26 @@
 import { ReactNode } from "react";
+import Title from "./Title";
 
 interface Props {
   title: string;
   subTitle: string;
+  url: string;
   icon: ReactNode;
   description?: ReactNode;
 }
 
-const ExperienceCard = ({ title, subTitle, icon, description }: Props) => {
+const ExperienceCard = ({ title, url, subTitle, icon, description }: Props) => {
   return (
     <div className="flex gap-4">
-      <span className="w-12 h-12 rounded-full bg-black border-[1px] border-blue-600 flex items-center justify-center">
+      <span className="w-12 h-12 rounded-full bg-black border-[1px] border-blue-600 flex items-center justify-center overflow-hidden">
         <span className="text-2xl">{icon}</span>
       </span>
       <div>
-        <h3 className="text-2xl font-bold">{title}</h3>
+        <h3 className="text-2xl font-bold">
+          <a href={url} target="_blank" >
+            {title}
+          </a>
+        </h3>
         <p className="text-base tracking-wide text-gray-400">{subTitle}</p>
         {description ? <span>{description}</span> : null}
       </div>
